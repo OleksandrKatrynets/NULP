1a59b1a01a40a1c8efc8ad7fe613bb6f51ea68bc
Alex Katrynets <itkerry6331@gmail.com>
2018-12-07 09:54:03 +0200

    Initialized solution projects

---
+++ NULP/NULP.Terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NULP.Terminal
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
